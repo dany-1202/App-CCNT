3d948106da06d96edc7662bfeb59535735cdc72d
Unstoppable1226 <joel.10joel.10@gmail.com>
2016-10-27 15:38:57 +0200

    Ajout du script de création de la base de données et Modifications de l'application angular
    quelques petites améliorations concernant angular pour faciliter la
    connexion à la base

---
+++ app/app.route.js
@@ -0,0 +1,25 @@
+(function(){
+/**
+ * Déclaration de l'application ctrlCCNT
+ */
+var ctrlCCNT = angular.module('ctrlCCNT', ['ngRoute','ngMaterial']);
+/**
+ * Configuration du module principal : ctrlCCNT
+ */
+ctrlCCNT.config(['$routeProvider',
+    function($routeProvider) { 
+        // Système de routage
+        $routeProvider
+        .when('/home', {
+            templateUrl: 'app/components/home/homeView.html',
+        })
+        .when('/connexion', {
+            templateUrl: 'app/components/connexion/connexionView.html',
+        })
+        .otherwise({
+            redirectTo: '/connexion'
+        });
+    }
+]);
+
+})();
---
+++ bdd/ccnt.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  localhost:8889
+-- G\'e9n\'e9r\'e9 le :  Jeu 27 Octobre 2016 \'e0 14:27
+-- Version du serveur :  5.6.33
+-- Version de PHP :  7.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Base de donn\'e9es :  `ccnt`\
+--
+CREATE DATABASE IF NOT EXISTS `ccnt` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `ccnt`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ccn_personne`
+--
+
+CREATE TABLE `ccn_personne` (
+  `per_id` int(11) NOT NULL,
+  `per_nom` varchar(50) NOT NULL,
+  `per_prenom` varchar(50) NOT NULL,
+  `per_mail` varchar(80) NOT NULL,
+  `per_mdp` varchar(50) NOT NULL,
+  `per_dateNaissance` date NOT NULL,
+  `per_adresse` varchar(50) NOT NULL,
+  `per_codePostal` int(11) NOT NULL,
+  `per_ville` varchar(50) NOT NULL,
+  `per_admin` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `ccn_personne`
+--
+
+INSERT INTO `ccn_personne` (`per_id`, `per_nom`, `per_prenom`, `per_mail`, `per_mdp`, `per_dateNaissance`, `per_adresse`, `per_codePostal`, `per_ville`, `per_admin`) VALUES
+(1, 'Bedonni', 'Jean-Pierre', 'jpb@gprh.ch', 'gprh', '1970-01-01', 'Rue de la batelle 5', 1205, 'Geneve', 1);
+
+--
+-- Index pour les tables export\'e9es
+--
+
+--
+-- Index pour la table `ccn_personne`
+--
+ALTER TABLE `ccn_personne`
+  ADD PRIMARY KEY (`per_id`);
+
+--
+-- AUTO_INCREMENT pour les tables export\'e9es
+--
+
+--
+-- AUTO_INCREMENT pour la table `ccn_personne`
+--
+ALTER TABLE `ccn_personne`
+  MODIFY `per_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
