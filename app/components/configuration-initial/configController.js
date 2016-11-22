f07b8f6a15ee4c7dbee961e26f44f3653d728d8b
Unstoppable1226 <joel.10joel.10@gmail.com>
2016-11-22 17:35:17 +0100

    Ajout des vues pour la configuration initial

--- app/components/configuration-initial/configController.js
+++ app/components/configuration-initial/configController.js
@@ -5,9 +5,17 @@
 **/
 var ctrlCCNT = angular.module('ctrlCCNT');
 
-ctrlCCNT.controller('configController', function($scope, $http, $location, $mdpDatePicker, $mdpTimePicker) {
+ctrlCCNT.controller('configController', function($scope, $http, $location, $mdpDatePicker, $mdpTimePicker, NotifService) {
   /* à mettre ce qu'on veut */
   $scope.currentDate = new Date();
+  $scope.currentView = 1;
+  $scope.departement = [{dep:"nom"}, {dep:"ici"}, {dep:"salut"}, {dep:"lfajkslfa"}];
+  $scope.pourcentage = 25;
+
+  this.change = function(ev, no) {
+    $scope.currentView = no;
+    $scope.pourcentage += 25;
+  }
 
 	this.showTimePicker = function(ev) {
     	$mdpTimePicker($scope.currentTime, {
@@ -15,12 +23,12 @@ ctrlCCNT.controller('configController', function($scope, $http, $location, $mdpD
       }).then(function(selectedDate) {
         $scope.currentTime = selectedDate;
       });;
-   }
+  }
    this.showTimePicker2 = function(ev) {
     	$mdpTimePicker($scope.currentTime2, {
         targetEvent: ev
       }).then(function(selectedDate) {
         $scope.currentTime2 = selectedDate;
       });;
-   } 
+  } 
 });
