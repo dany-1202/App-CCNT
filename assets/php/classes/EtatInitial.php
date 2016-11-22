3d5b7f0c34103e28d23bd46772871f0427def32c
Jallyy <vincent.jalley@gmail.com>
2016-11-22 20:11:37 +0100

    correction de bug dans l'api php état initial

--- assets/php/classes/EtatInitial.php
+++ assets/php/classes/EtatInitial.php
@@ -24,11 +24,11 @@ class InitialState {
 		$query = "INSERT INTO ccn_departement (`dep_nom`, `dep_eta_id`) VALUES (?, ?)";
 		if ($stmt = $db->prepare($query)) {
 			$stmt->bind_param('si', $data['nom'], $data['noEta']);
-		  $stmt->execute();
-		  if ($stmt->num_rows == 1) {
-		  	MySQLManager::close();
-		  	return true;
-		  }
+		  	$stmt->execute();
+		  	if ($stmt->num_rows == 1) {
+		  		MySQLManager::close();
+		  		return true;
+		  	}
 		}
 		MySQLManager::close();
 		return false;
@@ -40,14 +40,19 @@ class InitialState {
 		l'id du Departement
       Contraine BDD : le département identifié par son id doit existé dans la base
 	*/
-	public static function setDepartement (Departement $d) {
+    public static function setDepartement ($data) {
 		$db = MySQLManager::get();
-		$query = "UPDATE ccn_departement SET dep_nom = ? WHERE dep_id = ?";
+		$query = "INSERT INTO ccn_departement (`dep_nom`, `dep_eta_id`) VALUES (?, ?)";
 		if ($stmt = $db->prepare($query)) {
-			$stmt->bind_param('si', $d::getNom(), $d::getEtaId());
-		    $stmt->execute();
+			$stmt->bind_param('si', $data['depNom'], $data['etaId']);
+		  	$stmt->execute();
+		  	if ($stmt->num_rows == 1) {
+		  		MySQLManager::close();
+		  		return true;
+		  }
 		}
-	    MySQLManager::close();
+		MySQLManager::close();
+		return false;
 	}
 
 	/*Permet d'ajouter un Etablissement dans la table ccn_etablissement
@@ -55,26 +60,40 @@ class InitialState {
 		le nom, l'adresse, le teléphone de réservation, le téléphone de direction, l'email, 
 		le site web, le supplément d'adresse, le code postal, la localite, le nombre d'heure
 	*/
-	public static function insertEtablissement (Etablissement $e) {
-		$db = MySQLManager::get();
-		$query = "INSERT INTO ccn_etablissement (`dep_nom`, `dep_adresse`, `dep_telReservation`, `dep_telDirection`, `dep_email`, `dep_siteWeb`, `dep_adresseInfo`, `dep_codePostal`, `dep_localite`, `dep_nbHeure`) 
-				VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	public static function insertEtablissement ($data) {
+	    $db = MySQLManager::get();
+		$query = "INSERT INTO ccn_etablissement VALUES (-1, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 		if ($stmt = $db->prepare($query)) {
-			$stmt->bind_param('sssssssisi', $e::getNom(), $e::getAdresse(), $e::getTelReservation(), $e::getTelDirection(), $e::getEmail(), $e::getSiteWeb(), $e::getAdresseInfo(), $e::getCodePostal(), $e::getLocalite(), $e::getNbHeure());
-		    $stmt->execute();
+			$stmt->bind_param('sssssssisi', $data['nom'], $data['adresse'], $data['telReservation'], $data['telDirection'], $data['email'], $data['siteWeb'], $data['adresseInfo'], $data['codePostal'], $data['localite'], $data['nbHeure']);
+			$stmt->execute();
+			if ($stmt->num_rows == 1) {
+			  	MySQLManager::close();
+			  	return true;
+		  	}
 		}
-	    MySQLManager::close();
+		MySQLManager::close();
+		return false;
 	}
 
-//pas fonctionnelle encore
-	public static function insertPersonne (Personne $p) {
+	/*Permet d'ajouter un Departement dans la table ccn_departement
+	  En paramètre: une instance de Departement contenant :
+		le nom du Departement
+		l'id de l'Etablissement
+	  Contraine BDD : l'attribut etaId de l'instance de Departement doit être > 0 et existé dans la bdd
+	*/	
+	  public static function insertPersonne (Personne $p) {
 		$db = MySQLManager::get();
-		$query = "INSERT INTO ccn_personne VALUES (-1, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		$query = "INSERT INTO ccn_etablissement VALUES (-1, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 		if ($stmt = $db->prepare($query)) {
-			//$stmtUp->bind_param('sssssdssisissi', $e::getNom(), $e::getAdresse(), $e::getTelReservation(), $e::getTelDirection(), $e::getEmail(), $e::getSiteWeb(), $e::getAdresseInfo(), $e::getCodePostal(), $e::getLocalite(), $e::getNbHeure());
-		    $stmt->execute();
+			$stmt->bind_param('sssssdssisissi', $data['nom'], $data['prenom'], $data['mail'], $data['mdp'], $data['token'], $data['dateNaissance'], $data['adresse'], $data['infoSuppAdresse'], $data['codePostal'], $data['ville'], , $data['admin'], $data['telFixe'], $data['telMobile'], $data['depId'],);
+		  	$stmt->execute();
+		  	if ($stmt->num_rows == 1) {
+		  		MySQLManager::close();
+		  		return true;
+		  	}
 		}
-	    MySQLManager::close();
+		MySQLManager::close();
+		return false;
 	}
 
 }
