79783045c9777ccadd564050ce6d73ec94b0f694
Unstoppable1226 <joel.10joel.10@gmail.com>
2016-11-22 22:32:33 +0100

    Ajout de l'utilisateur qui n'est plus en clair

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -1,6 +1,7 @@
 var ctrlCCNT = angular.module('ctrlCCNT');
 
-ctrlCCNT.controller('homeController', function($scope, $http, $location) {
+ctrlCCNT.controller('homeController', function($rootScope, $scope, $http, $location, SessionService) {
+	$scope.user = {prenom: SessionService.get('user_prenom'), nom:SessionService.get('user_nom'), type:SessionService.get('user_type')}; // Reprend les données du rootScope (scope racine) tous les contrôleurs y ont accès
   $scope.lancerConfig = function () {
     $location.url("/config-init");
   }
