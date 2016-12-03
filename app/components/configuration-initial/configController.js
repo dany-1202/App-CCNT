33cdd69fc2a0ccd0ef0c24cc6afb618cb09f1f44
Unstoppable1226 <joel.10joel.10@gmail.com>
2016-12-03 21:49:59 +0100

    1er test

--- app/components/configuration-initial/configController.js
+++ app/components/configuration-initial/configController.js
@@ -39,8 +39,8 @@ ctrlCCNT.controller('configController', function($scope, $http, $location, $mdpD
                                 {id:3, type: 'text', name:'Adresse Infos +', value:null}, 
                                 {id:4, type: 'tel', name:'Tél. Réservation', value:null},
                                 {id:5, type: 'tel', name:'Tél. Direction', value:null},
-                                {id:6, type: 'email', name:'Email', value:null},
-                                {id:7, type: 'url', name:'Site Web', value:null},
+                                {id:6, type: 'text', name:'Email', value:null},
+                                {id:7, type: 'text', name:'Site Web', value:null},
                                 {id:8, type: 'number', name:'Code Postal', value:null},
                                 {id:9, type: 'text', name:'Localité', value:null},
                               ];
@@ -70,13 +70,58 @@ ctrlCCNT.controller('configController', function($scope, $http, $location, $mdpD
   }
 
   this.saveConfiguration = function() {
+   //test de l'ajout d'un établissement
+    var dataEtablissement = { 'nom': $scope.infoEtablissement[0].value, 
+                              'adresse': $scope.infoEtablissement[1].value, 
+                              'telReservation': $scope.infoEtablissement[3].value, 
+                              'telDirection': $scope.infoEtablissement[4].value, 
+                              'email': $scope.infoEtablissement[5].value, 
+                              'siteWeb': $scope.infoEtablissement[6].value, 
+                              'adresseInfo': $scope.infoEtablissement[2].value, 
+                              'codePostal': $scope.infoEtablissement[7].value, 
+                              'localite': $scope.infoEtablissement[8].value, 
+                              'nbHeure': $scope.nbHoursChosen};
+
+    var $res = $http.post("assets/php/insertEtablissement.php", dataEtablissement);
+    $res.then(function (message) {
+      var idEstablishment = message.data;
+      
+      
+        var dataInsertOuvertureInfo = {'jour': "lundi", 'debut': "2016-11-09 10:00:00", 'fin': "2016-11-09 12:00:00",'etaId': "1"};
+        var $res = $http.post("assets/php/insertOuvertureInfo.php", dataInsertOuvertureInfo);
+        $res.then(function (message) {
+          var dataFermetureInfo = {'date': "1992-05-31", 'etaId': "1"};
+          var $res = $http.post("assets/php/insertFermetureInfo.php", dataFermetureInfo);
+
+          $res.then(function (message) {
+          });
+        });
+      });      
+    });
+/*
+   
+*/
+/*
+    var dataInsertOuvertureInfo = {'jour': "lundi", 'debut': "2016-11-09 10:00:00", 'fin': "2016-11-09 12:00:00",'etaId': "1"};
+    var $res = $http.post("assets/php/insertOuvertureInfo.php", dataInsertOuvertureInfo);
+*/
+/* 
+    var dataPersonne = {'nom': "da Silva", 'prenom': "Joel", 'mail': "joel@gmail.com", 'mdp': "d8afab9d9d21a8906b16cb6eec67643602f7ecff38bc8dba1921d01a7c852b607df225ba1a0274f79b5d1b92ee2c45b4363d8f1fc84ebfba9bd245cdbb13ad98", 'token': "", 'dateNaissance': "1992-05-31", 'adresse': "thonex whsh", 'infoSuppAdresse': "fr", 'codePostal': "1221", 'ville': "thonex", 'admin': "1", 'telFixe': "026591651", 'telMobile': "419841", 'depId': "1", 'perGenre': "M"};
+    var $res = $http.post("assets/php/insertPersonne.php", dataPersonne);
+*/  
+    /*var data = {'nom': "Dep", 'noEta': "1"};
+    var $res = $http.post("assets/php/insertDepartement.php", data);
+    $res.then(function (message) {
+      
+      $location.path('/home'); */
+/*
     for (var i = 0; i < $scope.infoEtablissement.length; i++) {
       $scope.infoEtablissement[i]
       var $res = $http.post("assets/php/insertDepartement.php", data);
       $res.then(function (message) {
 
       });
-    };
+    }; */
     $location.path('/home');
     NotifService.success("Configuration-Initial","Tout vos paramètres ont bien été enregistrés");
   }  
