1cdaa33ebcc0d82190913df738a342b48e24ae6d
dany-1202 <dany.gomes.ifa@gmail.com>
2016-12-27 15:47:49 +0100

    Mise en page terminer
    Mais manque des soucis avec les controller …

--- app/components/configuration-employe/employeController.js
+++ app/components/configuration-employe/employeController.js
@@ -16,6 +16,14 @@ ctrlCCNT.controller('employeController', function($timeout, $rootScope, $scope, 
                     ];
   $scope.monHoraire = $scope.horaire[0];
 
+  $scope.pourcentage = [
+                    {name:"20%",value:0.20},{name:"25%",value:0.25},{name:"30%",value:0.30},{name:"35%",value:0.35},{name:"40%",value:0.40},
+                    {name:"45%",value:0.45},{name:"50%",value:0.50},{name:"55%",value:0.55},{name:"60%",value:0.60},{name:"65%",value:0.65},
+                    {name:"70%",value:0.70},{name:"75%",value:0.75},{name:"80%",value:0.80},{name:"85%",value:0.85},{name:"90%",value:0.90},
+                    {name:"95%",value:0.95},{name:"100%",value:1.0},
+                    ];
+  $scope.monPourc = $scope.pourcentage[0];
+
   $scope.contrat = [
                     {name:"Normal"},{name:'Apprentissage'},{name:'Personne externe'}
                     ];
@@ -28,17 +36,28 @@ ctrlCCNT.controller('employeController', function($timeout, $rootScope, $scope, 
                   ]; //Tableau contenant les employes
 
   $scope.showMen = false; // savoir si on doit afficher champs mensuel
-  $scope.showSpe = false; //
+  $scope.showSpe = false; // Savoir si on doit afficher champs spécial
 
+  $scope.nbHeure = 0; // nb d'heure spécifier dans le champs 
 
   $scope.ajouterEmploye = function () {
-    $scope.idUser = -1;
+    $scope.myEmp = null;
+    $rootScope.test = true;
     $location.url("/employe/edition");
+  }
+
+  $scope.retour = function () {
+    $location.url("/employe");
   }
 
   $scope.supEmploye = function(id) {
     //fonctionne : supression de l'utilisateur 
     $scope.employe.splice(id,1);
+  };
+
+  $scope.modEmploye = function(id) {
+    $scope.myEmp = $scope.employe[id];
+    $location.url("/employe/edition");
   };
 
   $scope.changementHoraire = function(){
