15d0b10d5ee29910672a427319f6e164e6348773
Unstoppable1226 <joel.10joel.10@gmail.com>
2017-04-18 18:40:40 +0200

    Mise en place des demandes

--- assets/php/accepterDemandesAPI.php
+++ assets/php/accepterDemandesAPI.php
@@ -12,7 +12,7 @@
 		echo("Vous n'avez pas le droit d'appeler cette requete ou requete invalide");
 	} else {
 		require_once("classes/DemandesDAO.php");
-		$res = DemandesDAO::accepterDemande($authData['demande']);
+		$res = DemandesDAO::traiterDemande($authData['demande']['idDem'], $authData['demande']['accept']);
 		echo(json_encode($res));
 	}
 	
